6ef5326baaffc87cbe5b1ffa40a4011b5ecd4e42
Rayssa Andrade <andraderay.dev@gmail.com>
2025-10-22 18:26:28 -0300

    Adicionando pasta backend com arquivos iniciais

---
+++ backend/src/main/java/br/com/confiaaqui/confia_aqui/ConfiaAquiApplication.java
@@ -0,0 +1,13 @@
+package br.com.confiaaqui.confia_aqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiaAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiaAquiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/confiaaqui/confia_aqui/ConfiaAquiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.confiaaqui.confia_aqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiaAquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
