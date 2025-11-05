84a0643ca2f8dff41424cf55a626c0e4ba987797
karimendes <karineferreiramendes@gmail.com>
2025-11-05 20:56:06 -0300

    feat: configuração de integração

--- frontend/confia-aqui-frontend/src/App.jsx
+++ frontend/confia-aqui-frontend/src/App.jsx
@@ -3,24 +3,30 @@ import Login from "./pages/Login"
 import RedefinirSenha from "./pages/RedefinirSenha"
 import Cadastro from "./pages/Cadastro"
 import Home from "./pages/Home"
+import HomeAdmin from "./pages/HomeAdmin"
 import Teste from "./pages/Teste"
 import Perfil from "./pages/Perfil"
-import './App.css'
-import './index.css'
+import "./App.css"
+import "./index.css"
+
+/*import { AuthProvider } from "./components/AuthContext"
+import PrivateRoute from "./components/PrivateRoute"*/
 
 function App() {
   return (
+    /*<AuthProvider>*/
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/redefinir-senha" element={<RedefinirSenha />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/teste" element={<Teste />} />
-        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/home" element={/*<PrivateRoute role="USER">*/<Home />/*</PrivateRoute>*/} /> 
+        <Route path="/teste" element={/*<PrivateRoute>*/<Teste />/*</PrivateRoute>*/} />
+        <Route path="/perfil" element={/*<PrivateRoute>*/<Perfil />/*</PrivateRoute>*/} />
       </Routes>
     </BrowserRouter>
+    /*</AuthProvider>*/
   );
 }
 
