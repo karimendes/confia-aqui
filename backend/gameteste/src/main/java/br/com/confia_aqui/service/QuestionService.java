1c7a9c35386731e714a3d6f33c1abe35af8a3172
bia nagae <138304130+beatriz-nagae@users.noreply.github.com>
2025-11-15 20:09:41 -0300

    UPDATE Crud Removed QuestionService.java
    Read Only

--- backend/gameteste/src/main/java/br/com/confia_aqui/service/QuestionService.java
+++ backend/gameteste/src/main/java/br/com/confia_aqui/service/QuestionService.java
@@ -2,15 +2,13 @@ package br.com.confia_aqui.service;
 
 import br.com.confia_aqui.model.Question;
 import br.com.confia_aqui.dao.QuestionDao;
-import br.com.confia_aqui.exception.QuestionNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.http.HttpStatus;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class QuestionService {
@@ -18,108 +16,43 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    // CRUD CREATE Pergunta
-    public ResponseEntity<String> addQuestion(Question question) {
-        try {
-            questionDao.save(question);
-            return new ResponseEntity<>("Pergunta adicionada com sucesso!", HttpStatus.CREATED);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Erro ao adicionar pergunta", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    // CRUD READ - ler todas as questoes
+    // CRUD removido. apenas leitura, busca todas as perguntas
     public ResponseEntity<List<Question>> getAllQuestions() {
         try {
             return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    // CRUD READ - ler por categoria
+    // CRUD removido. apenas leitura, busca as perguntas por categoria
     public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
         try {
-            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
-    }
-
-    // CRUD READ - buscacr peergunta pelo id 
-    public ResponseEntity<Question> getQuestionById(Integer id) {
-        try {
-            Optional<Question> question = questionDao.findById(id);
-
-            if (question.isEmpty()) {
-                throw new QuestionNotFoundException("Pergunta não encontrada com ID: " + id);
+            List<Question> questions = questionDao.findByCategory(category);
+            if (questions.isEmpty()) {
+                return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NOT_FOUND);
             }
-
-            return new ResponseEntity<>(question.get(), HttpStatus.OK);
-        } catch (QuestionNotFoundException e) {
-            throw e; // Deixa o GlobalExceptionHandler tratar
+            return new ResponseEntity<>(questions, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    // CRUD UPDATE - Editar a pergunta
-    public ResponseEntity<String> updateQuestion(Integer id, Question updatedQuestion) {
+    // CRUD removido. apenas leitura, lista as categorias
+    public ResponseEntity<List<String>> getAllCategories() {
         try {
-            Optional<Question> existingQuestion = questionDao.findById(id);
-
-            
-            if (existingQuestion.isEmpty()) {
-                throw new QuestionNotFoundException("Pergunta não encontrada com ID: " + id);
-            }
-
-            // atualiza/reescreve os campos
-            Question question = existingQuestion.get();
-            question.setQuestionTitle(updatedQuestion.getQuestionTitle());
-            question.setOption1(updatedQuestion.getOption1());
-            question.setOption2(updatedQuestion.getOption2());
-            question.setOption3(updatedQuestion.getOption3());
-            question.setOption4(updatedQuestion.getOption4());
-            question.setRightAnswer(updatedQuestion.getRightAnswer());
-            question.setDifficultyLevel(updatedQuestion.getDifficultyLevel());
-            question.setCategory(updatedQuestion.getCategory());
-
-            // save
-            questionDao.save(question);
-
-            return new ResponseEntity<>("Pergunta atualizada com sucesso!", HttpStatus.OK);
-
-        } catch (QuestionNotFoundException e) {
-            throw e; // Deixa o GlobalExceptionHandler tratar
+            List<Question> allQuestions = questionDao.findAll();
+            List<String> categories = allQuestions.stream()
+                    .map(Question::getCategory)
+                    .distinct()
+                    .sorted()
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(categories, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>("Erro ao atualizar pergunta", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    // CRUD DELETE - deletar pergunta
-    public ResponseEntity<String> deleteQuestion(Integer id) {
-        try {
-            Optional<Question> question = questionDao.findById(id);
-
-            if (question.isEmpty()) {
-                throw new QuestionNotFoundException("Pergunta não encontrada com ID: " + id);
-            }
-
-            // deleta
-            questionDao.deleteById(id);
-
-            return new ResponseEntity<>("Pergunta deletada com sucesso!", HttpStatus.OK);
-
-        } catch (QuestionNotFoundException e) {
-            throw e; // Deixa o GlobalExceptionHandler tratar
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Erro ao deletar pergunta", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
