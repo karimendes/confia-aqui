33ec2ffb8fff3673230f3bd7ce64323e3eeb00ca
Rayssa Andrade <andraderay.dev@gmail.com>
2025-11-20 00:24:38 -0300

    Renomeando pasta gamefication para game-service

--- backend/game-service/src/main/java/br/com/confia_aqui/GameServiceApplication.java
+++ backend/game-service/src/main/java/br/com/confia_aqui/GameServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GameficationApplication {
+public class GameServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GameficationApplication.class, args);
+		SpringApplication.run(GameServiceApplication.class, args);
 	}
 
 }
