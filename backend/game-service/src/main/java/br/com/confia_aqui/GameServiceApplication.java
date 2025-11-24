f742c8c9d8c5b70800da51adc7a95f8cb1d86cc1
beatriz-nagae <bh.prestashop@gmail.com>
2025-11-24 10:42:52 -0300

    FEATURE auth added on game-service: pom n security

--- backend/game-service/src/main/java/br/com/confia_aqui/GameServiceApplication.java
+++ backend/game-service/src/main/java/br/com/confia_aqui/GameServiceApplication.java
@@ -2,12 +2,14 @@ package br.com.confia_aqui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"br.com.confia_aqui", "br.com.commonlib"})
 public class GameServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GameServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GameServiceApplication.class, args);
+    }
 
 }
