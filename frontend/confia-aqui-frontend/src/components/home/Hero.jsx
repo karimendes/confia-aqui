9df41c0fb7a0f9ab257bb531adb7322f1c243560
Rayssa Andrade <andraderay.dev@gmail.com>
2025-11-25 19:39:35 -0300

    Atlteração do link

--- frontend/confia-aqui-frontend/src/components/home/Hero.jsx
+++ frontend/confia-aqui-frontend/src/components/home/Hero.jsx
@@ -8,9 +8,9 @@ function Hero() {
   <div className="relative text-start max-w-2xl px-4">
     <h2 className="text-4xl md:text-5xl font-bold mb-4">Passou por um golpe?</h2>
     <p className="text-xl font-normal mb-6">
-      Denuncie golpes diretamente aos canais oficiais e ajude as autoridades a combater fraudes de forma segura e eficaz.
+      Denuncie diretamente nos canais oficiais e ajude as autoridades a combater fraudes com segurança e eficácia.
     </p>
-    <a href="https://www.gov.br/cgu/pt-br/centrais-de-conteudo/campanhas/integridade-publica/denuncias" target="_blank" className="bg-azul hover:opacity-80 text-white hover:text-white px-7 py-4 rounded-2xl font-normal text-lg transition">
+    <a href="https://www.gov.br/pt-br/servicos/registrar-ocorrencia-policial-online" target="_blank" className="bg-azul hover:opacity-80 text-white hover:text-white px-7 py-4 rounded-2xl font-normal text-lg transition">
       Denunciar golpe
     </a>
   </div>
