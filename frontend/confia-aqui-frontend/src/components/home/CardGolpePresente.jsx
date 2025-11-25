7b753cb774c9689deade9761fd4bae05884554ff
karimendes <karineferreiramendes@gmail.com>
2025-11-25 15:23:08 -0300

    feat: novo card na home do usuário e componente de últimas notícias

---
+++ frontend/confia-aqui-frontend/src/components/home/CardGolpePresente.jsx
@@ -0,0 +1,33 @@
+function CardGolpePresente() {
+  return (
+    <section className="flex justify-center pt-10 pb-2 px-4 bg-gray-50">
+        <div className="max-w-5xl mx-auto py-12 px-4 mb-4 text-left bg-gray-50">
+            <h2 className="text-xl font-bold text-cinza-600 mb-5">Introdução ao Golpe do Presente</h2>
+
+            <h3 className="font-bold text-cinza-600 mb-2">O que é o Golpe do Presente?</h3>
+            <p className="text-cinza-600 mb-2 text-sm font-normal">
+            O Golpe do Presente é uma fraude em que golpistas oferecem um brinde, promoção ou “presente” falso para ganhar a confiança da vítima. <br /> O objetivo é levar a pessoa a compartilhar dados pessoais, informar códigos, acessar links falsos ou até pagar taxas que não existem. </p>
+            <p className="text-cinza-600 mb-5 text-sm font-normal">Ele funciona tão bem porque usa algo positivo — um presente — para esconder a intenção criminosa.</p>
+
+            <h3 className="font-bold text-cinza-600 mb-2">Como ocorre?</h3>
+            <p className="text-cinza-600 text-sm font-normal mb-2"> Os golpistas usam diferentes estratégias, como: </p>
+            <ul className="list-disc list-inside text-cinza-600 text-sm font-normal space-y-1 mb-2">
+              <li>Enviar mensagens fingindo ser lojas famosas oferecendo brindes.</li>
+              <li>Criar urgência: “Últimas unidades!”, “Resgate agora!”.</li>
+              <li>Solicitar dados, códigos de verificação ou documentos.</li>
+              <li>Cobrar uma falsa taxa de entrega.</li>
+              <li>Enviar links muito parecidos com sites reais, mas que são fraudulentos.</li>
+            </ul>
+            <p className="text-cinza-600 text-sm font-normal mt-2 mb-5 "> A tática é sempre a mesma: enganar, apressar e coletar informações.</p>
+
+            <h3 className="font-bold text-cinza-600 mb-2">Teste seus conhecimentos</h3>
+            <p className="text-cinza-600 mb-2 text-sm font-normal">
+            Descubra se você reconhece os principais sinais do Golpe do Presente. </p>
+            <p className="text-cinza-600 text-sm font-normal mb-2 ">Nosso teste é rápido e prático. Ele apresenta situações reais para você identificar o golpe na prática e aprender a agir com mais segurança. </p>
+            <p className="text-cinza-600 text-sm font-normal">Quanto mais você treina, mais preparado fica.</p>
+          </div>
+      </section>
+  )
+}
+
+export default CardGolpePresente
--- frontend/confia-aqui-frontend/src/pages/Home.jsx
+++ frontend/confia-aqui-frontend/src/pages/Home.jsx
@@ -4,13 +4,20 @@ import CardTeste from "../components/home/CardTeste";
 import FAQ from "../components/home/FAQ";
 import ChatBot from "../components/chatbot/ChatBot";
 import Footer from "../components/ui/Footer";
+import CardGolpePresente from "../components/home/CardGolpePresente";
+import UltimasNoticias from "../components/home/UltimasNoticias";
 
 function Home() {
   return (
     <div className="flex flex-col min-h-screen relative">
       <Header />
       <Hero />
+      <CardGolpePresente />
       <CardTeste />
+
+      <div className="max-w-5xl mx-auto">
+        <UltimasNoticias />
+      </div>
       <FAQ />
       <Footer />
 
